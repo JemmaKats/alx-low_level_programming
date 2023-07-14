65252cab4def998933eb834af300ca0eb957892e
Jerome <Jeromekats4@gmail.com>
2023-07-13 18:57:17 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the size of various typesa based on
+ * the computer it is compiled and run on..
+ * Return: Always 0.
+ */
+
+
+int main(void)
+
+{
+	printf("Size of a char: %zu byte(s)\,  sizeof(char));
