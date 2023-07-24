b3773151033b081a46602ee95ccbd04d916e4e76
Jerome <Jeromekats4@gmail.com>
2023-07-24 03:57:02 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
-int func(int *B[10]){
+void reset_to_98(int *);
 
-}
+/* Takes pointer to int
+ * update the value point to 98
+ */
 
-int main(void){
+int n;
 
-    int *B[10];
+{
+	n = 10
+	printf("%d\n", n);
+	reset_to_98(&n);
+	return (0);
 
-    func(&B);
-
-    return 0;
 }
